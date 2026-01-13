221dfc84aa468896bc55548fce2ca59489f32dac
Giancarlo <cooper458.gm@gmail.com>
2026-01-13 15:49:37 +0100

    fixed some errors

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ const ageInput = document.getElementById("age");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-  const km = number(kmInput.value);
-  const age = number(ageInput.value);
+  const km = Number(kmInput.value);
+  const age = Number(ageInput.value);
+
+  const pricePerKm = 0.21;
+  let price = km * pricePerKm;
+
+  if (age < 18) {
+    price = price * 0.8;
+  } else if (age > 65) {
+    price = price * 0.6;
+  }
+
+  console.log("Prezzo finale:", price.toFixed(2), "$");
 });
-
-const pricePerKm = 0.21;
-let price = km * pricePerKm;
-
-if (age < 18) {
-  price = price * 0.8;
-} else if (age > 65) {
-  price = price * 0.6;
-}
-
-console.log("Prezzo finale:", price.toFixed(2), "$");
