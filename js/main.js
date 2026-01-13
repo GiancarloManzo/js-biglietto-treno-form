80f3f9cd9f331482a10d845438f0f007b8ed8b2c
Giancarlo <cooper458.gm@gmail.com>
2026-01-13 15:33:33 +0100

    added eventlistener

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const kmInput = document.getElementById("km");
+const ageInput = document.getElementById("age");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const km = number(kmInput.value);
+  const age = number(ageInput.value);
+});
